a26d92cafe94032b2fd148bf379c48e7e0fbe757
Emanuel Saringan <dev@lambdageek.com>
2015-02-08 12:56:17 +0100

    Added Contest

---
+++ Contest/main.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+long GetScore(int p, int t) {
+  return max((p / 10) * 3, (p / 250) * t);
+}
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >>d;
+  long misha = GetScore(a, c);
+  long vasya = GetScore(b, d);
+  if (misha > vasya) {
+    cout << "Misha";
+  } else if (misha < vasya) {
+    cout << "Vasya";
+  } else {
+    cout << "Tie";
+  }
+  cout << endl;
+  return 0;
+}
